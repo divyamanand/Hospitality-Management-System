f16a646d9c2066c470461f13e5c14f219e4bdf5e
Divyam Anand <wrkdivan@gmail.com>
2024-07-18 15:51:03 +0530

    Allottment Algorithm

--- src/data/allotmentAlgo.js
+++ src/data/allotmentAlgo.js
@@ -1,3 +1,66 @@
-import { useData } from "./useData"
+const fullAllotment = (teams, hostels, gender) => {
+    teams.forEach(team => {
+        for (let hostel of hostels) {
+        if (hostel.Vacancy >= team[gender] && !team.Allotted) {
+            team.Rooms.push({
+            "Hostel Name": hostel["Hostel Name"],
+            'Room Number': hostel['Room Number'],
+            Members: team[gender],
+            });
+            team.Allotted = true;
+            hostel.Vacancy -= team[gender];
+            hostel.Teams.push({
+            'Group ID': team['Group ID'],
+            Members: team[gender],
+            });
+            break;
+        }
+        }
+    });
+};
 
-const {hostels, teams} = useData()
+const partialAllotment = (teams, hostels, gender) => {
+    teams.forEach(team => {
+        for (let hostel of hostels) {
+            console.log(hostel.Vacancy > 0 &&  typeof(team.Allotted) === "number" )
+            if (hostel.Vacancy > 0 && (typeof(team.Allotted) === "number")) {
+                const membersToAllot = Math.min(hostel.Vacancy, team[gender] - team.Allotted);
+                team.Rooms.push({
+                    "Hostel Name": hostel["Hostel Name"],
+                    'Room Number': hostel['Room Number'],
+                    Members: membersToAllot,
+                });
+                team.Allotted += membersToAllot;
+                hostel.Vacancy -= membersToAllot;
+                hostel.Teams.push({
+                    'Group ID': team['Group ID'],
+                    Members: membersToAllot,
+                });
+                if (team.Allotted === team[gender]) {
+                    team.Allotted = true;
+                    break;
+                }
+            }
+        }
+    });
+};
+
+const hasVacancy = (hostels) => {
+    return hostels.some(hostel => hostel.Vacancy > 0);
+};
+
+const allotRooms = (teams, hostels, gender) => {
+    const allHostels = hostels
+    const allTeams = teams
+
+    while (hasVacancy(allHostels) && allTeams.some(team => !team.Alloted)) {
+        fullAllotment(allTeams, allHostels, gender);
+        allHostels.sort((a, b) => b.Vacancy - a.Vacancy);
+        partialAllotment(allTeams, allHostels, gender);
+        allHostels.sort((a, b) => b.Vacancy - a.Vacancy);
+    }
+    
+    return {allTeams, allHostels}
+};
+
+export {allotRooms}
--- src/data/fromatData.js
+++ src/data/fromatData.js
@@ -1,74 +1,70 @@
-// const groups = [
-//     { GroupID: "1", "Total Members": "11", Gender: "5 Boys & 6 Girls" },
-//     { GroupID: "2", "Total Members": "7", Gender: "Girls" },
-//     { GroupID: "3", "Total Members": "5", Gender: "1 Girl & 6 Boys" },
-//     // Add other group objects here...
-//   ];
-//   const { boysGroups, girlsGroups } = convertGroups(groups);
-//   console.log("Boys Groups:", boysGroups);
-//   console.log("Girls Groups:", girlsGroups);
-
 
 const formatTeamsData = (groups) => {
     let boysGroups = [];
     let girlsGroups = [];
-  
+
     groups.forEach(group => {
-      const { Gender, ...rest } = group;
-      let boys = 0;
-      let girls = 0;
+        const groups = {...group, "Rooms": [], "Allotted": 0}
+        const {Gender, ...rest} = groups
+        let boys = 0;
+        let girls = 0;
 
-      const genderStr = Gender.toLowerCase();
-  
-      if (genderStr.includes("boys") || genderStr.includes("boy")) {
-        const boysMatch = genderStr.match(/(\d+)\s?boys?|\b(\d+)\s?boy\b/);
-        boys = boysMatch ? parseInt(boysMatch[1] || boysMatch[2], 10) : 0;
-      }
-  
-      if (genderStr.includes("girls") || genderStr.includes("girl")) {
-        const girlsMatch = genderStr.match(/(\d+)\s?girls?|\b(\d+)\s?girl\b/);
-        girls = girlsMatch ? parseInt(girlsMatch[1] || girlsMatch[2], 10) : 0;
-      }
-  
+        const genderStr = Gender.toLowerCase();
 
-      if (!boys && genderStr.includes("boys")) boys = parseInt(group['Total Members'], 10);
-      if (!girls && genderStr.includes("girls")) girls = parseInt(group['Total Members'], 10);
-  
-      if (boys > 0) {
-        boysGroups.push({
-          ...rest,
-          Gender: { Boy: boys }
-        });
-      }
-  
-      if (girls > 0) {
-        girlsGroups.push({
-          ...rest,
-          Gender: { Girl: girls }
-        });
-      }
-    });
-  
-    return { boysGroups, girlsGroups };
-  };
+        if (genderStr.includes("boys") || genderStr.includes("boy")) {
+            const boysMatch = genderStr.match(/(\d+)\s?boys?|\b(\d+)\s?boy\b/);
+            boys = boysMatch ? parseInt(boysMatch[1] || boysMatch[2], 10) : 0;
+        }
 
+        if (genderStr.includes("girls") || genderStr.includes("girl")) {
+            const girlsMatch = genderStr.match(/(\d+)\s?girls?|\b(\d+)\s?girl\b/);
+            girls = girlsMatch ? parseInt(girlsMatch[1] || girlsMatch[2], 10) : 0;
+        }
 
-const formatHostelsData = (hostels) => {
-    const girlsHostels = [];
-    const boysHostels = [];
+        if (!boys && genderStr.includes("boys")) boys = parseInt(group["Total Members"], 10);
+        if (!girls && genderStr.includes("girls")) girls = parseInt(group["Total Members"], 10);
 
-    hostelData.forEach(room => {
-        const { "Hostel Name": hostelName, ...rest } = room;
-        
-        if (room['Gender Accommodation'] === 'Girls') {
-            girlsHostels.push({ hostelName, ...rest });
-        } else if (room['Gender Accommodation'] === 'Boys') {
-            boysHostels.push({ hostelName, ...rest });
+        if (boys > 0) {
+            boysGroups.push({
+                ...rest,
+                Boys: boys,
+            });
+        }
+
+        if (girls > 0) {
+            girlsGroups.push({
+                ...rest,
+                Girls: girls,
+            });
         }
     });
 
-    return { girlsHostels, boysHostels };
+    boysGroups.sort((a, b) => b.Boys - a.Boys);
+    girlsGroups.sort((a, b) => b.Girls - a.Girls);
+
+    return { boysGroups , girlsGroups };
+};
+
+
+const formatHostelsData = (hostels, sorted = false) => {
+const girlsHostels = [];
+const boysHostels = [];
+
+hostels.forEach(room => {
+    const rooms = { ...room, "Vacancy": parseInt(room.Capacity), Capacity: parseInt(room.Capacity), Teams: [] };
+    const {Gender, Vacancy, ...rest} = rooms
+
+    if (Gender === "Girls") {
+        girlsHostels.push(rooms);
+    } else if (Gender === "Boys") {
+        boysHostels.push(rooms);
+    }
+});
+
+girlsHostels.sort((a, b) => parseInt(a.Vacancy) - parseInt(b.Vacancy));
+boysHostels.sort((a, b) => parseInt(a.Vacancy) - parseInt(b.Vacancy));
+
+return {girlsHostels: girlsHostels, boysHostels: boysHostels };
 }
 
-
-  export {formatTeamsData, formatHostelsData}
+export {formatTeamsData, formatHostelsData}
